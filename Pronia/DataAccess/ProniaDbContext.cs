8925d011f200022f8e50159967f1ad5f0bc4d853
mcloveen <129727789+mcloveen@users.noreply.github.com>
2023-07-31 00:36:58 +0400

    first

---
+++ Pronia/DataAccess/ProniaDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pronia.Models;
+
+namespace Pronia.DataAccess
+{
+	public class ProniaDbContext:DbContext
+	{
+        public DbSet<Slider> Sliders { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=MacDesktop;Database=Pronia;Username=sa;Password=said1234@");
+        }
+    }
+}
+
